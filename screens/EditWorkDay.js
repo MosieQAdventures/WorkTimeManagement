1f21e50f339fe5d41a0cd676dd1c3b39d1a928f2
Mosz <hubert.mosz@capgemini.com>
2023-02-15 19:34:10 +0100

    test commit

--- screens/EditWorkDay.js
+++ screens/EditWorkDay.js
@@ -89,7 +89,7 @@ export default function EditWorkDay({route, navigation}) {
     //console.log(changedDate.toString())
   };
 
-  // -------------------------------------
+  // ------------------------------------------
 
   useLayoutEffect(() => {
     navigation.setOptions({
